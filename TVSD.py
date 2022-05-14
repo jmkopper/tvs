20ae47f5f8c80607e9e081034eaaa55c419fbd7a
jmkopper <jay.kopper@gmail.com>
2022-05-13 22:20:39 -0500

    now reads and writes json files

--- TVSD.py
+++ TVSD.py
@@ -1,12 +1,8 @@
 from collections import Counter
 import heapq
-
-NNODES = 10
-SCALE_FACTOR = 1.1
-MAX_REDUNDANCY = 3
+import json
 
 """
-NNODES is the number of nodes in the graph
 SCALE_FACTOR is the amount by which the algorithm will allow itself to miss the target distance. A value of 1.x
 corresponds to x%. For example, if SCALE_FACTOR = 1.5 and target distance is 10, the algorithm will find routes
 with distance between 5 and 15 (inclusive.)
@@ -14,61 +10,52 @@ MAX_REDUNDANCY is the maximum number of times the algorithm will visit a given n
 computer on fire and is so dispreffered by the scoring algorithm that it's probably useless anyway.
 """
 
-# Galbraith gap data
-# adj = [[0, 6, 3.2, 0], [6, 0, 2.5, 2.5], [3.2, 2.5, 0, 2.2], [0, 2.5, 2.2, 0]]
-# eng = [[0, 0.05, 0.03, 0], [0, 0, 0, 0], [0, 0.04, 0, 0.03], [0, 0, 0.04, 0]]
+SCALE_FACTOR = 1.1
+MAX_REDUNDANCY = 3
 
-adj = [[0, 1.7, 0, 0.25, 0, 0, 0, 0, 2.7, 0],
-       [1.7, 0, 1.5, 0, 0, 0, 0, 0, 0, 1],
-       [0, 1.5, 0, 1.1, 0, 0, 0, 0, 0, 1.2],
-       [0.25, 0, 1.1, 0, 0.75, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0.75, 0, 2.7, 0, 0, 0, 0],
-       [0, 0, 0, 0, 2.7, 0, 3.7, 0, 0, 0],
-       [0, 0, 0, 0, 0, 3.7, 0, 0.8, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0.8, 0, 2.7, 0],
-       [2.7, 0, 0, 0, 0, 0, 0, 2.7, 0, 0],
-       [0, 1, 1.2, 0, 0, 0, 0, 0, 0, 0]
-       ]
+BASE_PATH = './'
+IMPORT_FILE_NAME = 'dutch_alvin.json'
+EXPORT_FILE_NAME = 'output.json'
 
-eng = [[0, 0, 0, 0, 0, 0, 0, 0, 0.03, 0],
-       [0, 0, 0.02, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0.03, 0, 0.01, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0.06, 0, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0.02, 0],
-       [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
-       [0, 0.04, 0.05, 0, 0, 0, 0, 0, 0, 0]
-       ]
+# Load from the json file
+with open(BASE_PATH + IMPORT_FILE_NAME) as file:
+        data = json.load(file)
+route_name = data['name']
+adj = data['distanceMatrix']
+eng = data['energyMatrix']
+start = data['start']
+end = data['end']
+target = data['target']
+NNODES = len(adj)
 
-if len(adj) != NNODES:
-    print('WARNING: adjacency matrix has length ', len(adj), ' but ', NNODES, 'nodes were expected.')
-if len(eng) != NNODES:
-    print('WARNING: energy matrix has length ', len(eng), ' but ', NNODES, 'nodes were expected.')
+
+# Verify the data
+assert len(adj) == len(eng)
 
 for n in adj:
-    if len(n) != NNODES:
-        print('WARNING: adj matrix row ', n, ' has length ', len(n))
+    assert len(n) == NNODES
 
 for n in eng:
-    if len(n) != NNODES:
-        print('WARNING: eng matrix row ', n, ' has length ', len(n))
+    assert len(n) == NNODES
 
 
+# Scrape the matrix to find neighbors of a given node
 def get_neighbors(n: int) -> list:
     return [i for i, x in enumerate(adj[n]) if x > 0]
 
 
+# Get the distance between two nodes
 def get_dist(u: int, v: int) -> float:
     d = adj[u][v]
     return d if d != 0 else float('inf')
 
 
+# Get the energy between two nodes
 def get_energy(u: int, v: int) -> float:
     return eng[u][v]
 
 
+# Compute the distance, energy, redundancy scores of a given path
 def get_path_scores(path: list) -> tuple[float, float, int]:
     distance, energy, redundancy = 0, 0, 0
     for i in range(len(path) - 1):
@@ -80,12 +67,14 @@ def get_path_scores(path: list) -> tuple[float, float, int]:
     return distance, energy, redundancy
 
 
+# Compute the total score of a path
 def total_score(path: list, target_d: float) -> float:
     d, e, r = get_path_scores(path)
     return (r ** 2) * (d / target_d) * (1 + e)
 
 
 # Main algorithm: Target Value Search D(FS)
+# Store result in a heap [score: float, path: list, distance score: float, energy score: float, redundancy score: int]
 def TVSD(n: int, end: int, target_dist: float, cur_path: list = None, cur_dist: float = 0):
     path_heap = []
     if not cur_path:
@@ -102,9 +91,21 @@ def TVSD(n: int, end: int, target_dist: float, cur_path: list = None, cur_dist: 
     return path_heap if path_heap else None
 
 
-# Sample operations
-target = 17
-heap = TVSD(0, 0, target)
+# Run the algorithm
+heap = TVSD(start, end, target)
+json_dictionary = {}
 
 for i in range(5):
-    print(heapq.heappop(heap))
+    line = heapq.heappop(heap)
+    current_dict = {
+        'score': round(line[0],2),
+        'path': line[1],
+        'distance': round(line[2],2)
+    }
+    json_dictionary['name'] = route_name
+    json_dictionary[i] = current_dict
+    print(line)
+
+json_object = json.dumps(json_dictionary, indent=4)
+with open(BASE_PATH + EXPORT_FILE_NAME, 'w') as file:
+    file.write(json_object)
